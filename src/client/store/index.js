143046f584944275b626d94c50cc41923e05788f
1Mill <erik.ekberg@outlook.com>
2019-06-22 13:17:56 -0700

    Update index.js

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -1,6 +1,6 @@
 export const actions = {
 	nuxtClientInit() {
 		// * Check that $peer object exists on page mount, otherwise redirect to homepage
-		if (!this.$peer.id) this.app.router.push('/');
+		if (!this.$peer.id) this.app.router.push('/')
 	},
-};
+}
