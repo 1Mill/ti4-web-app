bcc3840782acdc62ebd04a9e6b6e5ba1ce1751a6
1Mill <erik.ekberg@outlook.com>
2019-06-23 16:09:14 -0700

    Scaffold stratagy cards

---
+++ src/client/store/phaseStratagy.js
@@ -0,0 +1,44 @@
+export const state = () => ({
+	cards: [
+		{
+			initiative: 1,
+			name: 'leadership',
+			playerId: null,
+		},
+		{
+			initiative: 2,
+			name: 'diplomacy',
+			playerId: null,
+		},
+		{
+			initiative: 3,
+			name: 'politics',
+			playerId: null,
+		},
+		{
+			initiative: 4,
+			name: 'construction',
+			playerId: null,
+		},
+		{
+			initiative: 5,
+			name: 'trade',
+			playerId: null,
+		},
+		{
+			initiative: 6,
+			name: 'warfare',
+			playerId: null,
+		},
+		{
+			initiative: 7,
+			name: 'technology',
+			playerId: null,
+		},
+		{
+			initiative: 8,
+			name: 'imperial',
+			playerId: null,
+		},
+	],
+})
